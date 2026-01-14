7b7e7784480003db3a6b798534bfb69bf109dc63
codedbybhavya <bhavyadedhia0406@gmail.com>
2026-01-14 23:23:57 +0530

    Setup MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
@@ -8,6 +9,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Connect Database
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("ProCollab Backend Running");
